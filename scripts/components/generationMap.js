c1c239d6c93a085a45bbcfaff7cb2d21f245e41b
SuperJaba2000 <73773387+SuperJaba2000@users.noreply.github.com>
2021-11-05 19:36:20 +0300

    Add files via upload

---
+++ scripts/components/generationMap.js
@@ -0,0 +1,43 @@
+class Map {
+    constructor(){
+		this.materialArray;
+		
+		this.xoff = 0;
+		this.zoff = 0;
+		this.inc = 0.05;
+		this.amplitude = 30 + (Math.random() * 70);
+    }
+    generation(scene) {
+		const settings = new Settings();
+
+		const loader = new TextureLoader();
+		const materialArray = [side, , "dirt-top.jpg", "dirt-bottom.jpg", "dirt-side.jpg", "dirt-side.jpg"].map(v => {
+			let texture = loader.load(`/texture/${v}`);
+			texture.magFilter = NearestFilter;
+			texture.minFilter = NearestFilter;
+			return new MeshBasicMaterial({ map: texture });
+		})
+
+		this.materialArray = materialArray;
+
+		const geometry = new BoxGeometry( settings.blockSquare, settings.blockSquare, settings.blockSquare);
+
+		noise.seed(Math.random());
+		
+		for(let x = 0; x < settings.chunkSize; x++) {
+			for(let z = 0; z < settings.chunkSize; z++) {
+
+				let cube = new Mesh(geometry, materialArray);
+				
+
+				this.xoff = this.inc * x;
+				this.zoff = this.inc * z;
+				let y = Math.round(noise.simplex2(this.xoff, this.zoff) * this.amplitude / 5) * 5;
+
+				cube.position.set(x * settings.blockSquare, y, z * settings.blockSquare);
+				scene.add( cube );
+				
+			}
+		}
+	}
+}
---
+++ scripts/components/settings.js
@@ -0,0 +1,9 @@
+class Settings {
+	constructor() {
+		// площадь блока
+		this.blockSquare 		= 5;
+		// размер и площадь чанка
+		this.chunkSize 			= 16;
+		this.chunkSquare 		= this.chunkSize * this.chunkSize;
+	}
+}
