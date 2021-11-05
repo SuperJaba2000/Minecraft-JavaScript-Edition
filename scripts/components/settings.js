286acae6510ad2e6c0aead4dd314cf8fd335fe73
SuperJaba2000 <73773387+SuperJaba2000@users.noreply.github.com>
2021-11-05 20:42:12 +0300

    Update settings.js

--- scripts/components/settings.js
+++ scripts/components/settings.js
@@ -3,7 +3,7 @@ class Settings {
 		// площадь блока
 		this.blockSquare 		= 5;
 		// размер и площадь чанка
-		this.chunkSize 			= 16;
+		this.chunkSize 			= 48;
 		this.chunkSquare 		= this.chunkSize * this.chunkSize;
 	}
 }
