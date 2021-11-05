c777abfe3d5bf9eca0c1285c93955bd3c635f693
SuperJaba2000 <73773387+SuperJaba2000@users.noreply.github.com>
2021-11-05 22:05:46 +0300

    Update generationMap.js

--- scripts/components/generationMap.js
+++ scripts/components/generationMap.js
@@ -1,13 +1,44 @@
-class Map {
-    constructor(){
-		this.materialArray;
+class Map{
+	
+    tiles = [];
+	
+    constructor(seed){
+	this.materialArray = [];
+	   
+	this.SEED = seed;
 		
-		this.xoff = 0;
-		this.zoff = 0;
-	        this.yoff = 0;
-		this.inc = 0.05;
+	this.xoff = 0;
+	this.zoff = 0;
+	this.yoff = 0;
+        this.inc = 0.05;
     }
-    generation(scene) {
+	
+    generate(scene){
+        noise.seed(Math.random());
+		
+		for(let x = 0; x < settings.chunkSize; x++) {
+			for(let z = 0; z < settings.chunkSize; z++) {
+			    for(let y = 0; y < settings.worldHeight; y++) {
+
+				let cube = new Mesh(geometry, materialArray);
+				
+
+				this.xoff = this.inc * x;
+				this.zoff = this.inc * z;
+				this.yoff = this.inc * y;
+				    
+				let value = noise.simplex3(this.xoff, this.yoff, this.zoff);
+
+			if(value >= 0.5){
+			    cube.position.set(x * settings.blockSquare, y * settings.blockSquare, z * settings.blockSquare);
+			    scene.add(cube);
+			}
+	            }
+		}
+	    }
+	}
+	
+	rebuild(opt = false){
 		const settings = new Settings();
 
 		const loader = new TextureLoader();
@@ -20,29 +51,6 @@ class Map {
 
 		this.materialArray = materialArray;
 
-		const geometry = new BoxGeometry( settings.blockSquare, settings.blockSquare, settings.blockSquare);
-
-		noise.seed(Math.random());
-		
-		for(let x = 0; x < settings.chunkSize; x++) {
-			for(let z = 0; z < settings.chunkSize; z++) {
-			    for(let y = 0; y < 32; y++) {
-
-				let cube = new Mesh(geometry, materialArray);
-				
-
-				this.xoff = this.inc * x;
-				this.zoff = this.inc * z;
-				this.yoff = this.inc * y;
-				    
-				let value = noise.simplex3(this.xoff, this.yoff, this.zoff);
-
-				if(value >= 0.5){
-			            cube.position.set(x * settings.blockSquare, y * settings.blockSquare, z * settings.blockSquare);
-				    scene.add(cube);
-				}
-			    }
-			}
-		}
+		const geometry = new BoxGeometry(settings.blockSquare, settings.blockSquare, settings.blockSquare);
 	}
 }
